8d21e1fb786fc90e056a2282a70e5837a790526b
aksonai <fatjongerra@gmail.com>
2020-06-14 16:50:06 +0200

    [ADD] solution to dna-pairing

---
+++ js-algos-and-ds/intermediate-algos/dna-pairing.js
@@ -0,0 +1,28 @@
+/*
+Intermediate Algorithm Scripting: DNA Pairing
+The DNA strand is missing the pairing element. Take each character, 
+get its pair, and return the results as a 2d array. Base pairs are 
+a pair of AT and CG. Match the missing element to the provided 
+character. Return the provided character as the first element in 
+each array. 
+
+For example, for the input GCG, return [["G", "C"], 
+["C","G"],["G", "C"]]
+
+The character and its pair are paired up in an array, and all the 
+arrays are grouped into one encapsulating array.
+*/
+
+function pairElement(str) {
+    const pairings = {
+      "A": "T",
+      "T": "A",
+      "G": "C",
+      "C": "G"
+    }
+  
+    return str.split("").map(elem => [elem, pairings[elem]]);
+  }
+  
+  console.log(pairElement("GCG"));
+  

083d39e3733e163ef5948dee01dd536156dcd815
aksonai <fatjongerra@gmail.com>
2020-06-15 22:20:30 +0200

    [ADD] solution to convert-html-entities

---
+++ js-algos-and-ds/intermediate-algos/convert-html-entities.js
@@ -0,0 +1,28 @@
+/*
+Intermediate Algorithm Scripting: Convert HTML Entities
+
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), 
+in a string to their corresponding HTML entities.
+*/
+
+function convertHTML(str) {
+    const entities = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        "\"": "&quot;",
+        "'": "&apos;",
+    };
+    let convertedArr = str.split("").map(function (elem) {
+        if (entities.hasOwnProperty(elem)) {
+            return entities[elem];
+        } else {
+            return elem;
+        }
+    });
+    console.log(convertedArr);
+    return convertedArr.join('');
+}
+
+console.log(convertHTML("Hamburgers < Pizza < Tacos"));
+
